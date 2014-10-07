7bb5905c0f9ef2d9893d43cc8ddae9db26f0a52a
Maickell Vilela <maickell.vilela@gmail.com>
2014-10-07 00:16:30 -0400

    Initial Commit

---
+++ NY/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  NY
+//
+//  Created by Maickell Vilela on 07/10/14.
+//  Copyright (c) 2014 Maickell Vilela. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
