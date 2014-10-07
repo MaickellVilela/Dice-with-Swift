70ff8cac6ed8ff84e45b947bc614b174056d34c3
Maickell Vilela <maickell.vilela@gmail.com>
2014-10-07 01:12:53 -0400

    Reconhece o movimento e imprime um número aleatório de 0 a 6 na tela e no console.

--- NY/ViewController.swift
+++ NY/ViewController.swift
@@ -10,11 +10,25 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var result: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
+    override func motionEnded(motion: UIEventSubtype, withEvent event: UIEvent) {
+        
+        if event.subtype == UIEventSubtype.MotionShake {
+            
+            var aleatory = arc4random_uniform(5)
+            result.text = String(aleatory)
+            
+            println(result.text)
+            
+        }
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
